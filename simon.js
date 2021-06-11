9da65121bf91099167fb47054ba29c76bee40419
IgnacioGR <ignagilroca@gmail.com>
2021-06-11 22:40:11 +0200

    feat: :sparkles: answer checker

--- simon.js
+++ simon.js
@@ -3,6 +3,12 @@ let gamePattern = [];
 let userClickPattern = [];
 let level = 0;
 
+$("body").keydown(function (e) {
+  if (level === 0) {
+    nextSequence();
+  }
+});
+
 $(".btn").click(function (e) {
   e.preventDefault();
   let userClick = $(this).attr("id");
@@ -10,15 +16,19 @@ $(".btn").click(function (e) {
 
   makeSound(userClick);
   animatePress(userClick);
+
+  checkAnswer(userClickPattern.length - 1);
 });
 
 function nextSequence() {
+  userClickPattern = [];
+
+  level++;
+  $("h1").text("Level " + level);
+
   let random = Math.floor(Math.random() * 4);
   let randomChosenColor = buttonColors[random];
   gamePattern.push(randomChosenColor);
-  level++;
-
-  $("h1").text("Level " + level);
 
   $("#" + randomChosenColor)
     .fadeIn(100)
@@ -41,8 +51,22 @@ function animatePress(currentColor) {
   }, 100);
 }
 
-$("body").keydown(function (e) {
-  if (level === 0) {
-    nextSequence();
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickPattern[currentLevel]) {
+    if (userClickPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    let audio = new Audio("sounds/wrong.mp3");
+    audio.play();
+
+    $("body").addClass("game-over");
+
+    setTimeout(function () {
+      $("h1").text("You lost. Your score was " + level + ".");
+      $("body").removeClass("game-over");
+    }, 200);
   }
-});
+}
