d3da1fdf6a6af1154560de8ab40fc560e54ac382
IgnacioGR <ignagilroca@gmail.com>
2021-06-11 22:05:30 +0200

    feat: :sparkles: game start

--- simon.js
+++ simon.js
@@ -1,18 +1,48 @@
 let buttonColors = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
+let userClickPattern = [];
+let level = 0;
+
+$(".btn").click(function (e) {
+  e.preventDefault();
+  let userClick = $(this).attr("id");
+  userClickPattern.push(userClick);
+
+  makeSound(userClick);
+  animatePress(userClick);
+});
 
 function nextSequence() {
   let random = Math.floor(Math.random() * 4);
   let randomChosenColor = buttonColors[random];
   gamePattern.push(randomChosenColor);
+  level++;
+
+  $("h1").text("Level " + level);
 
   $("#" + randomChosenColor)
-    .fadeIn(50)
-    .fadeOut(50)
-    .fadeIn(50);
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
 
-  let audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+  makeSound(randomChosenColor);
+}
+
+function makeSound(color) {
+  let audio = new Audio("sounds/" + color + ".mp3");
   audio.play();
 }
 
-nextSequence();
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+$("body").keydown(function (e) {
+  if (level === 0) {
+    nextSequence();
+  }
+});
